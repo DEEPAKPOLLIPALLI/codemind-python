8a151ae8b12a5a27e8be5d6a86a6c22a1aab61bb
DEEPAKPOLLIPALLI <100121495+DEEPAKPOLLIPALLI@users.noreply.github.com>
2022-07-01 00:17:50 +0530

    def reverse(a):     rev=0     while a:         r=a%10         a=a//10         rev=rev*10+r     return rev def megaprime(n):     for i in range(2,(n//2)+1):         if n%i==0:             return 0     else:         d=0         p=0         while n:             r=n%10             n=n//10             d+=1             if r==1 or r==2 or r==3 or r==5 or r==7:                 p+=1         if p==d:             return 1         else:             return 0 def prime(n):     if n==1:         return 0     elif n==2:         return 1     for i in range(2,int(n*0.5)+2):         if n%i==0:             return 0     return 1 n=int(input()) rev=reverse(n) if prime(n)==0:     print('not prime') elif megaprime(n) and megaprime(rev):     print('circular prime') else:     print('prime but not a circular prime')

--- circular_prime.py
+++ circular_prime.py
@@ -1,21 +1,41 @@
+def reverse(a):
+    rev=0
+    while a:
+        r=a%10
+        a=a//10
+        rev=rev*10+r
+    return rev 
+def megaprime(n):
+    for i in range(2,(n//2)+1):
+        if n%i==0:
+            return 0
+    else:
+        d=0
+        p=0
+        while n:
+            r=n%10
+            n=n//10
+            d+=1
+            if r==1 or r==2 or r==3 or r==5 or r==7:
+                p+=1
+        if p==d:
+            return 1
+        else:
+            return 0
+def prime(n):
+    if n==1:
+        return 0
+    elif n==2:
+        return 1
+    for i in range(2,int(n*0.5)+2):
+        if n%i==0:
+            return 0
+    return 1
 n=int(input())
-c=0
-d=0
-for i in range(2,n) :
-    if(n%i==0) :
-        c=1
-rev=0
-while n!=0 :
-    r=n%10
-    rev=rev*10+r
-    n=n//10
-for i in range(2,rev) :
-    if(rev%i==0) :
-        d=1
-if c==0 and d==0 :
-    print("circular prime")
-elif c==0 and d!=0 :
-    print("prime but not a circular prime")
-else :
-    print("not prime")
-    
+rev=reverse(n)
+if prime(n)==0:
+    print('not prime')
+elif megaprime(n) and megaprime(rev):
+    print('circular prime')
+else:
+    print('prime but not a circular prime')
