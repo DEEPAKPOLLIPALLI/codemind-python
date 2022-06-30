3d098b86f29313ca5e5e51c71a55dde261466c67
DEEPAKPOLLIPALLI <100121495+DEEPAKPOLLIPALLI@users.noreply.github.com>
2022-06-30 10:19:46 +0530

    a=int(input()) fa=0 fb=1 fn=fa+fb c=0 for i in range(100):     if fa==a:         c=1         break     else:         fn=fa+fb         fa=fb         fb=fn         continue if c==1:     print("True") else:     print("False")

--- Fibonacci_or_not.py
+++ Fibonacci_or_not.py
@@ -1,18 +1,18 @@
-n=int(input())
-fa=c=0
+a=int(input())
+fa=0
 fb=1
-fn=0
-fib=[]
+fn=fa+fb
+c=0
 for i in range(100):
-    fib.append(fa)
-    fn=fa+fb
-    fa=fb
-    fb=fn
-for i in range(100):
-    if(n==fib[i]):
+    if fa==a:
         c=1
         break
-if(c==1):
-    print(True)
+    else:
+        fn=fa+fb
+        fa=fb
+        fb=fn
+        continue
+if c==1:
+    print("True")
 else:
-    print(False)
+    print("False")
