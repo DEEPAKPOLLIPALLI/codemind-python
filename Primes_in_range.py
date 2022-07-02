d5604b8ca7e37dae623eeded857667de568c6a1b
DEEPAKPOLLIPALLI <100121495+DEEPAKPOLLIPALLI@users.noreply.github.com>
2022-07-02 11:35:07 +0530

    def isprime(a):     if a<2:         return False     b=int(a**0.5)     for d in range(2,b+1):         if a%d==0:             return False     return True a=int(input()) b=int(input()) count=0 for i in range(a,b+1):     if isprime(i):         count+=1 print(count)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,14 +1,15 @@
-import math
-def prime(a):
-    c=0
-    for i in range(2,round(math.sqrt(a))+1):
-        if a%i==0:
-            return 0
-    return 1
+def isprime(a):
+    if a<2:
+        return False
+    b=int(a**0.5)
+    for d in range(2,b+1):
+        if a%d==0:
+            return False
+    return True
 a=int(input())
 b=int(input())
 count=0
 for i in range(a,b+1):
-    if prime(i)==1 and i!=1:
+    if isprime(i):
         count+=1
 print(count)
