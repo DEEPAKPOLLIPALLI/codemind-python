cde9fad53c3cefd093c2d5dc49626508ad845b4d
DEEPAKPOLLIPALLI <100121495+DEEPAKPOLLIPALLI@users.noreply.github.com>
2022-07-02 11:43:55 +0530

    n=int(input()) count=0 rev=0 for i in range(1,n+1):     if(n%i==0):         count+=1 if(count==2):     while(n!=0):         rem=n%10         rev=rev*10+rem         n//=10     fcount=0     for j in range(1,rev+1):         if(rev%j==0):             fcount+=1     if(fcount==2):         print("circular prime")     else:         print("prime but not a circular prime") else:     print("not prime")

--- circular_prime.py
+++ circular_prime.py
@@ -1,41 +1,21 @@
-def reverse(a):
-    rev=0
-    while a:
-        r=a%10
-        a=a//10
-        rev=rev*10+r
-    return rev 
-def megaprime(n):
-    for i in range(2,(n//2)+1):
-        if n%i==0:
-            return 0
-    else:
-        d=0
-        p=0
-        while n:
-            r=n%10
-            n=n//10
-            d+=1
-            if r==1 or r==2 or r==3 or r==5 or r==7:
-                p+=1
-        if p==d:
-            return 1
-        else:
-            return 0
-def prime(n):
-    if n==1:
-        return 0
-    elif n==2:
-        return 1
-    for i in range(2,int(n*0.5)+2):
-        if n%i==0:
-            return 0
-    return 1
 n=int(input())
-rev=reverse(n)
-if prime(n)==0:
-    print('not prime')
-elif megaprime(n) and megaprime(rev):
-    print('circular prime')
+count=0
+rev=0
+for i in range(1,n+1):
+    if(n%i==0):
+        count+=1
+if(count==2):
+    while(n!=0):
+        rem=n%10
+        rev=rev*10+rem
+        n//=10
+    fcount=0
+    for j in range(1,rev+1):
+        if(rev%j==0):
+            fcount+=1
+    if(fcount==2):
+        print("circular prime")
+    else:
+        print("prime but not a circular prime")
 else:
-    print('prime but not a circular prime')
+    print("not prime")
