0dc5ea7401e61ed32a1952b4b3eeefb235452586
DEEPAKPOLLIPALLI <100121495+DEEPAKPOLLIPALLI@users.noreply.github.com>
2022-07-02 18:51:24 +0530

    n=int(input()) a=list(map(int,input().split())) b=[] for i in a:     c=0     while(i):         x=i%10         i=i//10         c+=1     b.append(c) k=min(b) m=0 for j in a:     d=0     while(j):         y=j%10         j=j//10         d+=1     if(d==k):         m+=1 print(m)

--- Minimum_digit_elements.py
+++ Minimum_digit_elements.py
@@ -1,8 +1,23 @@
 n=int(input())
 a=list(map(int,input().split()))
-m=min(a)
-c=0
+b=[]
 for i in a:
-    if len(str(m))==len(str(i)):
+    c=0
+    while(i):
+        x=i%10
+        i=i//10
         c+=1
-print(c)
+    b.append(c)
+k=min(b)
+m=0
+for j in a:
+    d=0
+    while(j):
+        y=j%10
+        j=j//10
+        d+=1
+    if(d==k):
+        m+=1
+print(m)
+        
+        
