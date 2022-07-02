b5f0ceb7100367ac085f4abbeee1744f2fd47e91
DEEPAKPOLLIPALLI <100121495+DEEPAKPOLLIPALLI@users.noreply.github.com>
2022-07-02 18:39:47 +0530

    n,m=map(int,input().split()) a=list(map(int,input().split())) b=list(map(int,input().split())) a=set(a) b=set(b) c=[] for i in a:     if i not in b:         c.append(i) for j in b:     if j not in a:         c.append(j) print(len(c))

--- Count_elements_II.py
+++ Count_elements_II.py
@@ -1,17 +1,13 @@
-n , m = map(int,input().split())
-a = list(map(int,input().split()))
-b = list(map(int,input().split()))
-c = set(a)
-d = set(b)
-arr1 =[]
-arr2 = []
-arr1 = c
-arr2 = d
-count = 0
-for i in arr1:
-    if i not in arr2:
-        count+=1
-for i in arr2:
-    if i not in arr1:
-        count+=1
-print(count)
+n,m=map(int,input().split())
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+a=set(a)
+b=set(b)
+c=[]
+for i in a:
+    if i not in b:
+        c.append(i)
+for j in b:
+    if j not in a:
+        c.append(j)
+print(len(c))
