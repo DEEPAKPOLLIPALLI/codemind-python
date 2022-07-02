fc7e4b3c5dedd6a9d6780559de5ae8da211e777d
DEEPAKPOLLIPALLI <100121495+DEEPAKPOLLIPALLI@users.noreply.github.com>
2022-07-02 18:49:30 +0530

    n=int(input()) a=list(map(int,input().split())) for i in a:     sum=0     while(i>0):         r=i%10         i=i//10         sum=sum*10+r     print(sum,end=' ')

--- Reverse_game.py
+++ Reverse_game.py
@@ -1,11 +1,9 @@
-def rev(n):
-    rev=0
-    while n:
-        r=n%10
-        n//=10
-        rev=rev*10+r
-    return rev
 n=int(input())
 a=list(map(int,input().split()))
 for i in a:
-    print(rev(i),end=' ')
+    sum=0
+    while(i>0):
+        r=i%10
+        i=i//10
+        sum=sum*10+r
+    print(sum,end=' ')
