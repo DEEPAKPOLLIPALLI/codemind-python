9dd233af54a4906413b8673fec35b82ef6ec30ac
DEEPAKPOLLIPALLI <100121495+DEEPAKPOLLIPALLI@users.noreply.github.com>
2022-07-02 18:50:08 +0530

    n=int(input()) a=list(map(int,input().split())) sum=0 for i in a:     while(i>0):         r=i%10         i=i//10         sum=sum+r print(sum)

--- Play_with_digits.py
+++ Play_with_digits.py
@@ -1,13 +1,9 @@
-def rev(n):
-    rev=0
-    while n:
-        r=n%10
-        n//=10
-        rev+=r
-    return rev
 n=int(input())
 a=list(map(int,input().split()))
-s=0
+sum=0
 for i in a:
-    s+=rev(i)
-print(s)
+    while(i>0):
+        r=i%10
+        i=i//10
+        sum=sum+r
+print(sum)
